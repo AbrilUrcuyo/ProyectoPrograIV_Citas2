f0f51c9edf30908e2d3d72a27c9c7276068acdbf
AbrilUrcuyo <aurcuyoarce@gmail.com>
2025-05-29 00:18:10 -0600

    Funcionalidad de perfil medico completa. Un poco de funcionalidad de Lo de ver medicos, o el index

--- backend/src/main/java/org/example/backend/logic/Cita.java
+++ backend/src/main/java/org/example/backend/logic/Cita.java
@@ -1,5 +1,6 @@
 package org.example.backend.logic;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -23,6 +24,7 @@ public class Cita {
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "id_medico", nullable = false)
+    @JsonIgnore
     private Medico idMedico;
 
     @NotNull
